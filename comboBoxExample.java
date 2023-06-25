dd307947f1a8b668c43c040c7109c73f1a2d3689
bipana chapain <111263851+Chapainbipana@users.noreply.github.com>
2023-06-25 11:20:11 +0545

    Update comboBoxExample.java

--- comboBoxExample.java
+++ comboBoxExample.java
@@ -3,7 +3,7 @@ public class comboBoxExample {
 JFrame f;    
 comboBoxExample(){    
     f=new JFrame("ComboBox Example");    
-    String country[]={"India","Aus","U.S.A","England","Newzealand"};        
+    String country[]={"India","Nepal","U.S.A","England","Newzealand"};    
     JComboBox cb=new JComboBox(country);    
     cb.setBounds(50, 50,90,20);    
     f.add(cb);        
